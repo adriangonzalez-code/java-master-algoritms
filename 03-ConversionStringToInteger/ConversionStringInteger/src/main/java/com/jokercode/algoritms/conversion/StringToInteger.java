c5f32dc0eb42590acde13c997922f5c8457b19ad
Adrián González <g.adrian.gonzalez.90@gmail.com>
2022-08-11 00:35:27 -0500

    Adding conversion from String to Integer value algoritm

---
+++ 03-ConversionStringToInteger/ConversionStringInteger/src/main/java/com/jokercode/algoritms/conversion/StringToInteger.java
@@ -0,0 +1,39 @@
+package com.jokercode.algoritms.conversion;
+
+public class StringToInteger {
+
+    public static void main(String[] args) {
+        System.out.println(toInteger("-2020"));
+        System.out.println(toInteger("2020"));
+        System.out.println(toInteger("5sd6"));
+    }
+
+    public static int toInteger(String str) {
+        int result = 0;
+        boolean negative = false;
+
+        for (int n = 0; n < str.length(); n++) {
+            char ch = str.charAt(n);
+
+            if ((n == 0) && (ch == '-')) {
+                negative = true;
+                continue;
+            }
+
+            if (!isDigit(ch))
+                return 0;
+
+            int digit = ch - '0';
+            result = result * 10 + digit;
+        }
+
+        if (negative)
+            return -result;
+
+        return result;
+    }
+
+    public static boolean isDigit(char ch) {
+        return (ch >= '0') && (ch <= '9');
+    }
+}
